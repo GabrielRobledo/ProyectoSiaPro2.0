82c4468c1ecab27a5ebb52dc900c6329e7634195
GabrielRobledo <40006480+GabrielRobledo@users.noreply.github.com>
2026-09-09 08:43:53 -0300

    Update conexion.js

--- backend/db/conexion.js
+++ backend/db/conexion.js
@@ -1,15 +1,19 @@
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'db_siap'
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || '',
+  database: process.env.DB_NAME || 'db_siap',
+  port: process.env.DB_PORT || 3306,
+  ssl: {
+    rejectUnauthorized: false
+  }
 });
 
 connection.connect((err) => {
   if (err) throw err;
-  console.log('Conectado a la base de datos db_siap');
+  console.log('Conectado a la base de datos de Aiven correctamente');
 });
 
 module.exports = connection;
