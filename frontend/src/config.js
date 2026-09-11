64b6f902bf161ecc2a819b1f6ef950ae15426d49
GabrielRobledo <lsi.rga@gmail.com>
2026-09-11 12:32:33 -0300

    Actualizacion con el modulo de update de periodo funcional totalmente.

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,3 +1,3 @@
-const API_URL = 'http://localhost:3000'; // Cambia esto según tu configuración de backend
+const API_URL = import.meta.env.VITE_API_URL || 'https://proyectosiapro2-0.onrender.com'; // Cambia esto según tu configuración de backend
 
 export default API_URL;
